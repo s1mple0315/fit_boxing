b5ca596a06bf8488ba6af15d1a1a7a719ebc80c7
s1mple0315 <145556641+s1mple0315@users.noreply.github.com>
2025-04-01 17:50:01 +0500

    refactor MobileSchedulePage; enhance layout and styles for schedule content and button, integrate Link for navigation

--- src/pages/MobileSchedulePage/MobileSchedulePage.jsx
+++ src/pages/MobileSchedulePage/MobileSchedulePage.jsx
@@ -1,12 +1,20 @@
 // src/pages/MobileSchedulePage/MobileSchedulePage.jsx
+import { Link } from "react-router-dom";
 import styles from "./MobileSchedulePage.module.css";
 
 const MobileSchedulePage = () => {
   return (
     <div className={styles.mobileSchedulePage}>
       <section className={styles.scheduleSection}>
-        <h1>Мобильное расписание занятий</h1>
-        <p>Удобное расписание занятий в вашем телефоне.</p>
+        <div className={styles.scheduleContent}>
+          <h1>Мобильное расписание занятий</h1>
+          <p>Удобное расписание занятий в вашем телефоне.</p>
+        </div>
+
+        <div className={styles.scheduleButton}>
+          <img src="assets/pages/buttonIcon.png" alt="" />
+          <Link>Записаться на пробное занятие</Link>
+        </div>
       </section>
     </div>
   );

09e7c368baae9f5347554065c4393bca12ca2efe
s1mple0315 <145556641+s1mple0315@users.noreply.github.com>
2025-04-01 18:10:23 +0500

    refactor ServicesPage; update layout and styles for services section, integrate dynamic card components and navigation link

--- src/pages/ServicesPage/ServicesPage.jsx
+++ src/pages/ServicesPage/ServicesPage.jsx
@@ -1,24 +1,36 @@
-import TrainingHall from "../../components/TrainingHall/TrainingHall";
-import trainingHalls from "../../data/trainingHalls";
+import { Link } from "react-router-dom";
+
 import styles from "./ServicesPage.module.css";
+import pageData from "../../data/pages";
+import Card from "../../components/Card/Card";
 
 const ServicesPage = () => {
   return (
     <div className={styles.servicesPage}>
       <section className={styles.servicesSection}>
-        {trainingHalls.map((hall) => (
-          <TrainingHall
-            key={hall.title}
-            title={hall.title}
-            description={hall.description}
-            area={hall.area}
-            trainingDirections={hall.trainingDirections}
-            benefits={hall.benefits}
-            image={hall.image}
-            layout={hall.layout}
-            contentType={hall.contentType}
-          />
-        ))}
+        <div className={styles.serviceContent}>
+          <h1>Мобильное расписание занятий</h1>
+          <p>Удобное расписание занятий в вашем телефоне.</p>
+        </div>
+
+        <div className={styles.serviceButton}>
+          <img src="assets/pages/buttonIcon.png" alt="" />
+          <Link>Записаться на пробное занятие</Link>
+        </div>
+      </section>
+
+      <section className={styles.cardsSection}>
+        {pageData.map((page) => {
+          return (
+            <Card
+              key={page.slug}
+              title={page.title}
+              description={page.description}
+              url={`/${page.slug}`}
+              slug={page.slug}
+            />
+          );
+        })}
       </section>
     </div>
   );

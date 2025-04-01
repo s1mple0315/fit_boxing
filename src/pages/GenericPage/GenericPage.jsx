6e2dc0056039317e7e9f72816a481b16a6a0f054
s1mple0315 <145556641+s1mple0315@users.noreply.github.com>
2025-04-01 17:19:39 +0500

    refactor GenericPage component; update styles and layout for header and button; integrate Link for navigation

--- src/pages/GenericPage/GenericPage.jsx
+++ src/pages/GenericPage/GenericPage.jsx
@@ -2,6 +2,7 @@ import BorderedCard from "../../components/BorderedCard/BorderedCard";
 import Stage from "../../components/Stages/Stages";
 import PageTopDescription from "../../components/PageTopDescription/PageTopDescription"; // Импортируем PageTopDescription
 import styles from "./GenericPage.module.css";
+import { Link } from "react-router-dom";
 
 const GenericPage = ({ pageData }) => {
   const page = pageData[0];
@@ -11,7 +12,6 @@ const GenericPage = ({ pageData }) => {
   }
 
   const imageURL = `assets/pages/${page.image}.png`;
-  // Для страниц из groupPages добавляем фоновое изображение и кнопку "Записаться на пробное занятие"
   const renderPageTopDescription = () => {
     if (page.cards || page.stages) {
       return (
@@ -19,47 +19,29 @@ const GenericPage = ({ pageData }) => {
           style={{
             backgroundImage: `url(${imageURL})`,
           }}
-          className={styles.pageHeader}
+          className={`${styles.pageHeader} d-flex flex-column`}
         >
-          <h1 className={styles.headerTitle}>{page.topDescription.title}</h1>
-          <p className={styles.headerDescription}>
-            {page.topDescription.description}
-          </p>
+          <div className={styles.headerContent}>
+            <h1 className={styles.headerTitle}>{page.topDescription.title}</h1>
+            <p className={styles.headerDescription}>
+              {page.topDescription.description}
+            </p>
+          </div>
           {page.button && (
-            <a href={page.button.url} className={styles.headerButton}>
-              {page.button.text}
-            </a>
+            <div className={styles.headerButton}>
+              <img src="assets/pages/buttonIcon.png" alt="" />
+              <Link to={page.button.link}>{page.button.text}</Link>
+            </div>
           )}
         </section>
       );
     }
-
-    // Для страниц из pageData без фона, только серый цвет
-    return (
-      <PageTopDescription
-        backgroundImage={"assets/pages/pageTop.png"}
-        backgroundClass="bg-light" // Для pageData фон серый
-        layout="default"
-      >
-        <h1 style={{ backgroundColor: "white" }}>
-          {page.topDescription.title}
-        </h1>
-        <p>{page.topDescription.description}</p>
-        {page.button && (
-          <a href={page.button.url} className={styles.headerButton}>
-            {page.button.text}
-          </a>
-        )}
-      </PageTopDescription>
-    );
   };
 
   return (
     <div className={styles.pageContent}>
-      {/* Стартовый блок с описанием страницы */}
       {renderPageTopDescription()}
 
-      {/* Content Section: Either Stages or Cards */}
       {page.cards && page.cards.length > 0 ? (
         <section className={styles.featuresSection}>
           <div className={styles.featuresGrid}>

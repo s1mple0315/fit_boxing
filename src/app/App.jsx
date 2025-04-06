8ae36e4bf5ee0976e99c5f84880f457c416462e4
s1mple0315 <145556641+s1mple0315@users.noreply.github.com>
2025-04-07 02:13:11 +0500

    add @vercel/speed-insights dependency and integrate SpeedInsights component in App

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import AppRouter from "../config/routes/router";
 
 const App = () => {
   return (
     <div>
       <AppRouter />
+      <SpeedInsights />
     </div>
   );
 };
